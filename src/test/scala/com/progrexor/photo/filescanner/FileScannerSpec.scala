ca382e9a7d27220cb1c39deec505417c6710d0a8
Andrey Dmitriev <biosetup@gmail.com>
2018-01-07 19:01:50 +0000

    FileScannerSpec added

---
+++ src/test/scala/com/progrexor/photo/filescanner/FileScannerSpec.scala
@@ -0,0 +1,15 @@
+package com.progrexor.photo.filescanner
+
+import org.scalatest.{FlatSpec, GivenWhenThen}
+
+class FileScannerSpec extends FlatSpec with GivenWhenThen {
+
+  behavior of "FileScanner"
+
+  it should "scan subfolders from provided path for given extensions and create file list" in {}
+  // https://docs.oracle.com/javase/tutorial/displayCode.html?code=https://docs.oracle.com/javase/tutorial/essential/io/examples/Find.java
+
+
+  it should "search for duplicates by filesize and then by hash" in {}
+
+}
